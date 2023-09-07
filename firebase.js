e34dad1ef7af78a08540760fdde85a5fb12ffd65
Shotta_501 <92776143+RW-501@users.noreply.github.com>
2023-09-07 12:44:25 -0500

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -4,14 +4,15 @@ import firebase from 'firebase/app';
 import 'firebase/auth';
 import 'firebase/firestore';
 
-const firebaseConfig = {
-  apiKey: "YOUR_API_KEY",
-  authDomain: "YOUR_AUTH_DOMAIN",
-  projectId: "YOUR_PROJECT_ID",
-  storageBucket: "YOUR_STORAGE_BUCKET",
-  messagingSenderId: "YOUR_MESSAGING_SENDER_ID",
-  appId: "YOUR_APP_ID"
-};
+  const firebaseConfig = {
+    apiKey: "AIzaSyCt2g6HmhffcrVvsH3urZynlJq_fLL0_A8",
+    authDomain: "wefamily-44c0b.firebaseapp.com",
+    projectId: "wefamily-44c0b",
+    storageBucket: "wefamily-44c0b.appspot.com",
+    messagingSenderId: "469288138698",
+    appId: "1:469288138698:web:773a2e1f17f2f392f38133",
+    measurementId: "G-XNM9BJZ3F7"
+  };
 
 firebase.initializeApp(firebaseConfig);
 
