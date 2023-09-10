66ddb000577a67eda0a2577570d951048f6c16ac
Shotta_501 <92776143+RW-501@users.noreply.github.com>
2023-09-10 17:39:25 -0500

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -70,7 +70,10 @@ if (typeof firebase !== 'undefined' && typeof firebase.firestore === 'function')
         console.log(` userLoggedIn `,userLoggedIn);
 
     if (userLoggedIn === true) {
+              console.log(` auth.currentUser `,auth.currentUser);
+
         return auth.currentUser;
-    }
+    }else{
     return null;
+    }
 }
