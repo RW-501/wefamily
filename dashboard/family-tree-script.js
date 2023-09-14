f5bc90187667c6ad3167dce6a14a5c5bfb978c9b
Shotta_501 <92776143+RW-501@users.noreply.github.com>
2023-09-13 21:31:33 -0500

    Update family-tree-script.js

--- dashboard/family-tree-script.js
+++ dashboard/family-tree-script.js
@@ -149,6 +149,104 @@ root.children.push(memberData.id);
 }
 
 
+function fetchFamilyMemberData(collectionName, treeID) {
+    return new Promise((resolve, reject) => {
+        const db = firebase.firestore();
+
+        // Fetch data from Firestore
+        db.collection(collectionName)
+            .where('familyID', 'array-contains', treeID)
+            .get()
+            .then((querySnapshot) => {
+                // Initialize the root object with the correct child ID
+                const root = {
+                    id: treeID, // A unique identifier for the root node
+                    name: "McClure Family", // Set the root node's name (you can change this)
+                    children: [], // Initialize an empty children array
+                };
+
+                // Create a map to store member data by ID
+                const memberDataMap = {};
+
+                // Build the tree starting from the root
+function buildTree(node) {
+    // Create an array to store all related members (children, parents, siblings, spouses)
+    const relatedMembers = [];
+
+    // Add children to the array
+    relatedMembers.push(...(node.children || []));
+
+    // Add parents to the array
+    relatedMembers.push(...(node.parents || []));
+
+    // Add siblings to the array
+    relatedMembers.push(...(node.siblings || []));
+
+    // Add spouses to the array
+    relatedMembers.push(...(node.spouse || []));
+
+    // Recursively build the tree for each related member
+    node.children = relatedMembers
+        .map((relatedMemberID) => {
+            const relatedMemberNode = memberDataMap[relatedMemberID];
+            if (relatedMemberNode) {
+                return buildTree(relatedMemberNode);
+            }
+            return null; // Handle the case where relatedMemberNode is undefined or missing
+        })
+        .filter(Boolean); // Filter out null values
+
+    return node;
+}
+
+
+                querySnapshot.forEach((doc) => {
+                    const docData = doc.data();
+                    const id = doc.id;
+                    const name = `${docData.first_name} ${docData.last_name}`;
+                    const photo = docData.photo || '';
+                    const children = docData.children || [];
+                    const spouse = docData.spouse || [];
+                    const parents = docData.parents || [];
+                    const siblings = docData.sibling || [];
+
+                    // Create an object for the current member
+                    const memberData = {
+                        id: id,
+                        name: name,
+                        photo: photo,
+                        children: children,
+                        spouse: spouse,
+                        parents: parents,
+                        siblings: siblings,
+                        // You can add more properties here if needed
+                    };
+ //   console.log("memberData   " + memberData.name);
+
+                    countChild++;
+                    if(countChild === 1){
+root.children.push(memberData.id);
+                    }
+                    // Store member data in the map
+                    memberDataMap[id] = memberData;
+                });
+
+                // Build the tree starting from the root
+                const hierarchicalTree = buildTree(root);
+
+                // Resolve the promise with the hierarchical tree structure
+                resolve(hierarchicalTree);
+            })
+            .catch((error) => {
+                reject(error);
+            });
+    });
+}
+
+
+
+
+
 function loadFamilyTreeChart() {
     console.log("currentFamilyID   " + currentFamilyID);
     
