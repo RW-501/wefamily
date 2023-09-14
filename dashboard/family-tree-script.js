fb98b8ddf694d6c40f913350b5efbc46cf030893
Shotta_501 <92776143+RW-501@users.noreply.github.com>
2023-09-14 01:32:01 -0500

    Update family-tree-script.js

--- dashboard/family-tree-script.js
+++ dashboard/family-tree-script.js
@@ -145,7 +145,7 @@ chartGroup.selectAll("path")
     });*/
 
 // Draw custom links between nodes
-chartGroup.selectAll(".circle")
+chartGroup.selectAll("path")
     .data(links)
     .enter()
     .append("path")
