36b6e6a660e71137ddc70b9140f5a3b651f6eab0
Shotta_501 <92776143+RW-501@users.noreply.github.com>
2023-09-15 06:02:39 -0500

    Update family-tree-script.js

--- dashboard/family-tree-script.js
+++ dashboard/family-tree-script.js
@@ -26,8 +26,10 @@ zoomOutButton.addEventListener('click', () => {
 function generateFamilyTreeChart(familyData) {
 	
     console.log("generateFamilyTreeChart   " + familyData);
+	    console.log("maxChildDepth   " + maxChildDepth);
+
 const width = window.screen.width;
-    const height = 200 * maxChildDepth; // Height of the chart
+    const height = 1500; // Height of the chart
 
     // Create an SVG element to contain the chart
     const svg = d3.select("#family-tree-area")
