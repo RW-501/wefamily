50bd4c3435b95b96a64afc3384bc52c1c3a6f288
Shotta_501 <92776143+RW-501@users.noreply.github.com>
2023-09-18 21:01:36 -0500

    Update family-tree-script.js

--- dashboard/family-tree-script.js
+++ dashboard/family-tree-script.js
@@ -37,7 +37,7 @@ zoomInButton.addEventListener('click', () => {
 function generateFamilyTreeChart(familyData) {
 	
 
-const width = 800;// window.screen.width;
+let width = 800;// window.screen.width;
     const height = 1500 ;//*  maxHierarchyDepth; // Height of the chart
 document.getElementById('family-tree-area').innerHTML ="";
     // Create an SVG element to contain the chart
