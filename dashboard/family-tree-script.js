32a3ec946a2d62e9b88c7e74ab15638e8973fc63
Shotta_501 <92776143+RW-501@users.noreply.github.com>
2023-09-23 01:49:24 -0500

    Update family-tree-script.js

--- dashboard/family-tree-script.js
+++ dashboard/family-tree-script.js
@@ -116,7 +116,49 @@ chartGroup.selectAll("path")
 
 
 
+	// Add text for each node
+nodeGroup.selectAll("text")
+    .attr("x", 0)  // Adjust the x position as needed
+    .attr("y", -10)  // Adjust the y position as needed
+    .attr("dy", 70)
+    .attr("text-anchor", "middle")
+    .style("font-weight", "900")
+    .style("font-size", "1.2em")
+    .style("fill", "white")
+    .style("pointer-events", "none")
+    .text(d => d.data.name)
+    .each(function () {
+        const bbox = this.getBBox();
+        d3.select(this.parentNode)
+            .insert("rect", ":first-child")
+            .attr("x", bbox.x - 5)
+            .attr("y", bbox.y - 2)
+            .attr("width", bbox.width + 10)
+            .attr("height", bbox.height + 4)
+            .attr("rx", 10)
+            .attr("ry", 10)
+            .style("fill", "black")
+            .style("opacity", 1);  // Adjust the opacity as needed
+    });
+
+
+
 	
+
+
+
+
+
+/*
+	
+	// Adjust the position of the text elements
+nodeGroup.selectAll("text")
+    .attr("x", d => d.x)  // Adjust the x position as needed
+    .attr("y", d => d.y - 20);  // Adjust the y position as needed
+
+*/
+
+
     let memberData = familyData;
 console.log('memberData data:', memberData.data);
 memberData = memberData.data;
@@ -187,48 +229,6 @@ handleCollisions(nodes);
 */
 
 
-
-// Add text for each node
-nodeGroup.selectAll("text")
-    .attr("x", 0)  // Adjust the x position as needed
-    .attr("y", -10)  // Adjust the y position as needed
-    .attr("dy", 70)
-    .attr("text-anchor", "middle")
-    .style("font-weight", "900")
-    .style("font-size", "1.2em")
-    .style("fill", "white")
-    .style("pointer-events", "none")
-    .text(d => d.data.name)
-    .each(function () {
-        const bbox = this.getBBox();
-        d3.select(this.parentNode)
-            .insert("rect", ":first-child")
-            .attr("x", bbox.x - 5)
-            .attr("y", bbox.y - 2)
-            .attr("width", bbox.width + 10)
-            .attr("height", bbox.height + 4)
-            .attr("rx", 10)
-            .attr("ry", 10)
-            .style("fill", "black")
-            .style("opacity", 1);  // Adjust the opacity as needed
-    });
-
-
-
-	
-
-
-
-
-
-/*
-	
-	// Adjust the position of the text elements
-nodeGroup.selectAll("text")
-    .attr("x", d => d.x)  // Adjust the x position as needed
-    .attr("y", d => d.y - 20);  // Adjust the y position as needed
-
-*/
 
 
 
