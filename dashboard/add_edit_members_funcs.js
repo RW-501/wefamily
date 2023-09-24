b9631bfa21d2373687641976d8e436890d78f77b
Shotta_501 <92776143+RW-501@users.noreply.github.com>
2023-09-23 22:45:57 -0500

    Update add_edit_members_funcs.js

--- dashboard/add_edit_members_funcs.js
+++ dashboard/add_edit_members_funcs.js
@@ -302,13 +302,19 @@ document.getElementById('add-member-type').addEventListener('submit', function (
 
 	    
     function addParent(member) {
+
+	    	if(getParentNames(member.memberID) == null){
+// console.log('setRootValue memberID.',memberID);
+
                   //  console.log('member:'+ member);
         // Add parent logic here
 showAddMemberTypePopup("Parent");
 	    addFamily_MemberID = member.memberID;
 	    addFamily_Type = 'Parent';
 addFamily_MemberData = member;
-
+}else{
+openCreateTreePopup();
+}
     }
 
  
